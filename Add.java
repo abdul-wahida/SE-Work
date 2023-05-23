2dfc7afab37b2a2a492a93ad19c555e5c9579f39
Pervaz-Chachar <134380742+Pervaz-Chachar@users.noreply.github.com>
2023-05-23 15:33:47 +0500

    secound commit

---
+++ Add.java
@@ -0,0 +1,7 @@
+// Addition class
+
+class Add {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
---
+++ Sub.java
@@ -0,0 +1,6 @@
+// Subtraction class
+class Sub {
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+}
